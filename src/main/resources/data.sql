741c534764db99d4098fe14d4b98c5f8c3bd057c
Bassem Salman <bassem.salman3@gmail.com>
2023-12-22 17:47:24 +0200

    Create Tables if they dont exist

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,63 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS supermarket;
+
+-- Use the created database
+USE supermarket;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS client (
+    client_id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS driver (
+    driver_id INT AUTO_INCREMENT PRIMARY KEY,
+    drivername VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS supplier (
+    supplier_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    driver_id INT,
+    FOREIGN KEY (driver_id) REFERENCES driver(driver_id)
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS item (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    itemname VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    supplier_id INT,
+    FOREIGN KEY (supplier_id) REFERENCES supplier(supplier_id)
+);
+
+CREATE TABLE IF NOT EXISTS purchase (
+    purchase_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    totalprice DECIMAL(10, 2) NOT NULL,
+    client_id INT,
+    employee_id INT,
+    FOREIGN KEY (client_id) REFERENCES client(client_id),
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+);
+
+CREATE TABLE IF NOT EXISTS purchase_item (
+    purchase_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    purchase_id INT,
+    item_id INT,
+    FOREIGN KEY (purchase_id) REFERENCES purchase(purchase_id),
+    FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
+
+-- Insert data into tables
 INSERT INTO client (email, fname, lname)
 VALUES ('john@example.com', 'John', 'Doe'),
        ('jane@example.com', 'Jane', 'Smith');
@@ -25,8 +85,8 @@ VALUES ('2023-05-01 09:00:00', 35.97, 1, 1),
        ('2023-05-03 16:45:00', 21.98, 1, 2);
 
 INSERT INTO purchase_item (purchase_id, item_id)
-VALUES (1, 4),
-       (1, 4),
-       (2, 5),
-       (3, 6),
-       (3, 6);
+VALUES (1, 1),
+       (1, 1),
+       (2, 2),
+       (3, 3),
+       (3, 3);
